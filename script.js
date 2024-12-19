34a12b7c10de4d3419c721a1ffc076d9c43d0f75
qingdanm <qingdanm@qq.com>
2024-12-20 00:43:59 +0800

    update

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-MathJax.Hub.Config({
-    config: ["MMLorHTML.js"],
-    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-    extensions: ["MathMenu.js", "MathZoom.js"]
-  });
-
 const storedPassword = localStorage.getItem('password');
     if (storedPassword!== 'a') {
         let enteredPassword;
